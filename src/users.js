cc4baa9fd425dbd92e546bbdf9703e13ebf3804f
Shyam Rithul R S <shyamrithulrenju1234@gmail.com>
2026-01-25 16:31:37 +0530

    new user added

--- src/users.js
+++ src/users.js
@@ -1,10 +1,11 @@
 const users = [
-  { username: "arathi", password: "1234" },
+  { username: "arathi", password: "psc123" },
   { username: "binsy", password: "psc123" },
   { username: "sreejilesh", password: "psc123" },
   { username: "pranav", password: "psc123" },
   { username: "deepu", password: "psc123" },
   { username: "vivek", password: "psc123" },
+  { username: "shamla", password: "psc123" },
 ];
 
 export default users;
